7b9ac63a1f36b3a80dc0715ab1f37d551816065a
dieuanh14 <vuongtrandieuanh14@gmail.com>
2023-10-12 20:54:34 +0700

    course-content-page

--- src/pages/courses/CourseContent.jsx
+++ src/pages/courses/CourseContent.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
 
-function CourseContent() {
-  return (
-	<div>CourseContent</div>
-  )
-}
-
-export default CourseContent
+export const CourseContent = () => {
+  const location = useLocation();
+  const { state } = location || {};
+  const selectedCourse = state && state.selectedCourse;
+  console.log(selectedCourse);
+  if (selectedCourse) {
+    return (
+      <div>
+        <h2>Course Content for {selectedCourse.courseName.name}</h2>
+        <p>Date: {selectedCourse.courseName.date}</p>
+        <p>{selectedCourse.courseName.content}</p>
+        {/* You can display other content details here */}
+      </div>
+    );
+  } else {
+    // Handle the case where the course data is not available
+    return (
+      <div>
+        <h2>Course Not Found</h2>
+        <p>The requested course could not be found.</p>
+      </div>
+    );
+  }
+};
